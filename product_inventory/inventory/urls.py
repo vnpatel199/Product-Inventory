7683e938de17c4a2f3be9a1da2beb963e9bd0b2b
Vashishtha Patel <om199.patel@gmail.com>
2022-08-10 18:54:03 +1000

    update Ajax search bar and add css for output

--- product_inventory/inventory/urls.py
+++ product_inventory/inventory/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('',  views.index, name='index'),
-    path('search/',views.search, name='search')
+    path('search/',views.search, name='search'),
+    path('login', views.Login, name='login')
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
--- product_inventory/inventory/views.py
+++ product_inventory/inventory/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login
+from django.http import Http404
 
 
 # Create your views here.
@@ -11,8 +13,29 @@ def index(request):
 @csrf_exempt
 def search(request):
     if 'term' in request.GET:
+        res = None
         qs = Product.objects.filter(product_name__icontains=request.GET.get('term'))
-        titles = list()
+        titles = []
         for i in qs:
-            titles.append(i.product_name)
-        return JsonResponse(titles, safe=False)
+            item = {
+                'name':i.product_name,
+                'price': i.price,
+                'image':i.photo.url
+            }
+            titles.append(item)
+        res = titles
+        return JsonResponse({'data':res})
+
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('')
+        else:
+            raise Http404("Bad Credentials")
+            
+    return render(request, "login.html")
