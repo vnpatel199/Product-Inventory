8a8481ccc9ee373fab4b8e5a20e5966a562e6af9
Vashishtha Patel <om199.patel@gmail.com>
2022-08-15 01:05:02 +1000

    add login, home and update page and views

--- product_inventory/inventory/urls.py
+++ product_inventory/inventory/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -6,7 +6,11 @@ from django.conf import settings
 urlpatterns = [
     path('',  views.index, name='index'),
     path('search/',views.search, name='search'),
-    path('login', views.Login, name='login')
+    path('login/', views.Login, name='login'),
+    path('logout/', views.Logout, name='logout'),
+    path('home/', views.home, name='home'),
+    path('home/<pk>/', views.detail, name='detail'),
+    path('home/update/<pk>/', views.update, name='update')
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
--- product_inventory/inventory/views.py
+++ product_inventory/inventory/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+import re
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -34,8 +36,40 @@ def Login(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('')
+            return redirect('home')
         else:
             raise Http404("Bad Credentials")
             
     return render(request, "login.html")
+
+def Logout(request):
+    logout(request)
+    return redirect('index')
+
+
+@login_required(login_url='/login/')
+def home(request):
+    product = Product.objects.all()
+    print(product)
+    return render(request, "home.html",{
+        "product": product
+    })
+
+@login_required(login_url='/login/')
+def detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "detail.html", {'product':product})
+
+@login_required(login_url='/login/')
+def update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        product_name = request.POST['product_name']
+        quantity = request.POST['quantity']
+        price = request.POST['price']
+        update = Product.objects.filter(pk=pk).update(product_name=product_name,quantity=quantity ,price=price)
+        return redirect('home')
+
+    return render(request, "update.html", {'product':product})
+
+
